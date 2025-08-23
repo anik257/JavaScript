8d1f83b9e4ca36764e2efbfc8a7e6ce7b336a7de
Ajaj Anwar Anik <ajaj.anik180@gmail.com>
2025-08-23 20:49:45 +0600

    added problem-5

---
+++ Assignment-3/problem-5.js
@@ -0,0 +1,5 @@
+/** Problem 05 - ( PH Email Generator )  */ 
+var student= { name: "jhankar" , roll: 1014 ,department: "cse" }; 
+//write your code here 
+var email = student.name + student.roll + "."+student.department+"@Path2D.call.bd";
+console.log(email);
