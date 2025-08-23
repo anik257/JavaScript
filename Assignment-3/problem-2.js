a1251c45bb5b728a53b6e6e6c62d6b3900a2cff5
Ajaj Anwar Anik <ajaj.anik180@gmail.com>
2025-08-23 18:47:03 +0600

    added problem-2

---
+++ Assignment-3/problem-2.js
@@ -0,0 +1,12 @@
+/** Problem -02 ( Cycle or Laptop ) */
+var money = 10000;
+//write your code here
+if (money>=25000){
+    console.log('Buy a laptop')
+} else if (money<=10000){
+    console.log('Buy a Cycle')
+}
+else 
+{
+    console.log('Buy a Chocolate')
+}
