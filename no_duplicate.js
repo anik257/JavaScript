3810b86608d3387a76398609fb3380849ce29959
Ajaj Anwar Anik <ajaj.anik180@gmail.com>
2025-11-13 12:42:37 +0600

    added unique array

---
+++ no_duplicate.js
@@ -0,0 +1,16 @@
+const studentName = [ 'Anik', 'Rahim', 'Karim', 'Anik', 'Sofik', 'Karim'];
+const number = [12, 23, 22, 12, 23];
+
+function noDuplicate(array){
+    const unique = [];
+    for (const item of array){
+        if (unique.includes(item) === false){
+            unique.push(item);
+        }
+
+        }   
+        
+        return unique;
+}
+const uniqueArray = noDuplicate(number);
+console.log(uniqueArray)
