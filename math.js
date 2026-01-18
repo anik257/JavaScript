d474840f99d8943d314ab96cf5a0c189ad5bc98c
Ajaj Anwar Anik <ajaj.anik180@gmail.com>
2026-01-18 14:56:08 +0600

    added random and rand

--- math.js
+++ math.js
@@ -6,6 +6,12 @@ console.log(max);
 
 console.log(Math.PI);
 console.log(Math.abs(100-70));
-console.log(Math.round(2.25))
-console.log(Math.round(2.78))
-console.log(Math.floor(2.98))
+console.log(Math.round(2.25));
+console.log(Math.round(2.78));
+console.log(Math.floor(2.98));
+console.log(Math.random());
+console.log(Math.random()*10);
+const rand = Math.round (Math.random()*10)
+console.log(rand);
+
+
